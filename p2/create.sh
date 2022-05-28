106493474a9726077651e0b6a5cc316cbb93a9f0
Oussama Seqqam <ouseqqam@e2r6p14.1337.ma>
2022-05-28 16:54:34 +0100

    edit p2

--- p2/create.sh
+++ p2/create.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --flannel-iface=eth1
-kubectl apply -f /vagrant/deployment.yml
-kubectl apply -f /vagrant/service.yml
-kubectl apply -f /vagrant/ingress.yml
+/usr/local/bin/kubectl apply -f /vagrant/deployment.yml
+/usr/local/bin/kubectl apply -f /vagrant/service.yml
+/usr/local/bin/kubectl apply -f /vagrant/ingress.yml
