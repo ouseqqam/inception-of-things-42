9a6632a7ef132851b66da12752756fde5ea75f9d
Oussama SEQQAM <ouseqqam@student.1337.ma>
2022-05-28 22:01:52 +0100

    edit p2

--- p2/create.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --flannel-iface=eth1
-/usr/local/bin/kubectl apply -f /vagrant/deployment.yml
-/usr/local/bin/kubectl apply -f /vagrant/service.yml
-/usr/local/bin/kubectl apply -f /vagrant/ingress.yml
--- p2/script.sh
+++ p2/script.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-
 curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644 --flannel-iface=eth1
+/usr/local/bin/kubectl apply -f /vagrant/deployment.yml
+/usr/local/bin/kubectl apply -f /vagrant/service.yml
+/usr/local/bin/kubectl apply -f /vagrant/ingress.yml
